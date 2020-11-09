153941129502fcea9b650f712d43985822cad413
unknown <alisha5dias@gmail.com>
2020-11-09 19:18:36 +0530

    commit

--- public/default.js
+++ public/default.js
@@ -9,14 +9,14 @@ var socket = io();
 var url_string=(window.location.href).toLowerCase();
 var url = new URL(url_string);
   
-var countdownEl=document.getElementById("countdown")
-var countdownEl2=document.getElementById("countdown2")
+//var countdownEl=document.getElementById("countdown")
+//var countdownEl2=document.getElementById("countdown2")
 var startMinutes=url.searchParams.get("time");
 var startMinutes2=url.searchParams.get("opponenttime");
 let time= startMinutes * 60;
 let opponenttime=startMinutes2 * 60;
 
-function countdown(){
+/*function countdown(){
   setInterval(function(){
     if (countdownEl<=0){
       clearInterval(countdownEl=0)
@@ -40,6 +40,100 @@ function countdown2(){
     opponenttime--;
   },1000)
 }
+*/
+var current_time = Date.parse(new Date());
+var deadline = new Date(current_time + startMinutes*60*1000);
+var deadline2 = new Date(current_time + startMinutes2*60*1000);
+
+  function time_remaining(endtime){
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor( (t/1000) % 60 );
+    var minutes = Math.floor( (t/1000/60) % 60 );
+    var hours = Math.floor( (t/(1000*60*60)) % 24 );
+    var days = Math.floor( t/(1000*60*60*24) );
+    return {'total':t, 'days':days, 'hours':hours, 'minutes':minutes, 'seconds':seconds};
+  }
+//for timer1  
+  var timeinterval;
+  function run_clock(id,endtime){
+    var clock = document.getElementById(id);
+    function update_clock(){
+      var t = time_remaining(endtime);
+      clock.innerHTML = t.minutes+':'+t.seconds;
+      if(t.total<=0){ clearInterval(timeinterval); }
+    }
+    update_clock(); // run function once at first to avoid delay
+    timeinterval = setInterval(update_clock,1000);
+  }
+  
+  
+  var paused = false; // is the clock paused?
+  var time_left; // time left on the clock when paused
+  
+  function pause_clock(){
+    if(!paused){
+      paused = true;
+      clearInterval(timeinterval); // stop the clock
+      time_left = time_remaining(deadline).total; // preserve remaining time
+    }
+  }
+  
+  function resume_clock(){
+    if(paused){
+      paused = false;
+  
+      // update the deadline to preserve the amount of time remaining
+      deadline = new Date(Date.parse(new Date()) + time_left);
+  
+      // start the clock
+      run_clock('clockdiv',deadline);
+    }
+  }
+  // handle pause and resume button clicks
+  document.getElementById('pause').onclick = pause_clock;
+  document.getElementById('resume').onclick = resume_clock;  
+
+//for timer2
+var timeinterval2;
+
+function run_clock2(id,endtime){
+	var clock2 = document.getElementById(id);
+	function update_clock2(){
+		var t = time_remaining(endtime);
+		clock2.innerHTML = t.minutes+':'+t.seconds;
+		if(t.total<=0){ clearInterval(timeinterval2); }
+	}
+	update_clock2(); // run function once at first to avoid delay
+	timeinterval2 = setInterval(update_clock2,1000);
+}
+
+run_clock('clockdiv2',deadline2);
+
+function pause_clock2(){
+	if(!paused){
+		paused = true;
+		clearInterval(timeinterval2); // stop the clock
+		time_left = time_remaining(deadline2).total; // preserve remaining time
+	}
+}
+
+
+function resume_clock2(){
+	if(paused){
+		paused = false;
+
+		// update the deadline to preserve the amount of time remaining
+		deadline2 = new Date(Date.parse(new Date()) + time_left);
+
+		// start the clock
+		run_clock2('clockdiv2',deadline2);
+	}
+}
+
+
+document.getElementById('pause2').onclick = pause_clock2;
+document.getElementById('resume2').onclick = resume_clock2;
+
 
 var initGame = function(){
   
@@ -54,9 +148,7 @@ var initGame = function(){
     };
     board = new ChessBoard('gameBoard',cfg);
     game = new Chess();
-    updateStatus();
-    
-    
+
 };
 
 var handleMove =function(source,target){
@@ -68,13 +160,15 @@ var handleMove =function(source,target){
       if (game.turn() === 'b') {
       moveColor = 'Black'
   
-      if (moveColor='White'){
-        countdown();
-      }
-      else {
-        countdown2();
-      } 
-    }}
+      
+    }
+    if (moveColor='White'){
+      run_clock('clockdiv',deadline);
+    }
+    else {
+      run_clock('clockdiv2',deadline2);
+    } 
+  }
 };
 
 //called when the server calls socket.broadcast('move')
